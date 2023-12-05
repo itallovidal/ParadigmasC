2a910a6cd4cf7074a6e4af726b45db47d98e68ff
italloVidal <itallovidalll@gmail.com>
2023-12-04 22:08:28 -0300

    users

---
+++ cmake-build-debug/CMakeFiles/ParadigmasC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParadigmasC.dir/main.c.obj"
+  "CMakeFiles/ParadigmasC.dir/main.c.obj.d"
+  "ParadigmasC.exe"
+  "ParadigmasC.exe.manifest"
+  "ParadigmasC.pdb"
+  "libParadigmasC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ParadigmasC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
